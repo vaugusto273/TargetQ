d93f2c68a5bbd6bb1d369c48b5dc4490faf5b11a
vaugusto273 <59668167+vaugusto273@users.noreply.github.com>
2024-09-03 15:32:36 -0300

    :white_check_mark: Fifth Question :tada:

---
+++ JS/Q5.js
@@ -0,0 +1,4 @@
+function Q5() {
+    document.getElementById("Q5Resul").innerHTML = 
+    "<ol><li>Ligo o primeiro interruptor e então espero por um tempo, após esperar eu desligo e ligo o segundo interruptor e e então vou para a primeira sala</li><li>Após chegar na primeira sala vejo se a lampadas estiver ligada, se não estiver vejo se está quente ou fria, se estiver fria sei que é o terceiro interruptor, se estiver quente sei que é o primeiro e se estiver acessa sei que é o segundo</li><li>Então volto, e com oque descobri anotado eu vou para a segunda sala e faço o mesmo processo de perguntas</li><li>Assim terei a minha conclusão</li><li style='color: white;'>Achei fácil :sunglasses:</li></ol>"
+}
